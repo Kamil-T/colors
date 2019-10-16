f132c5891e84484f8e09e563efc3e286068ae68b
Kamil T <trzaskomakamil@gmail.com>
2019-10-16 22:27:36 +0200

    Updates

---
+++ precache-manifest.72962edede525fe9f55a8a2fdc40f505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e99ea5df61ecb49b27dcd4b917026c",
+    "url": "/colors/index.html"
+  },
+  {
+    "revision": "2859179ba6645ba389a0",
+    "url": "/colors/static/css/2.96479aa2.chunk.css"
+  },
+  {
+    "revision": "da6d9f31fd05ec96e27d",
+    "url": "/colors/static/css/main.de328308.chunk.css"
+  },
+  {
+    "revision": "2859179ba6645ba389a0",
+    "url": "/colors/static/js/2.a1f043bd.chunk.js"
+  },
+  {
+    "revision": "da6d9f31fd05ec96e27d",
+    "url": "/colors/static/js/main.bbe663e8.chunk.js"
+  },
+  {
+    "revision": "86e82da3496d33444588",
+    "url": "/colors/static/js/runtime-main.93bca2ca.js"
+  }
+]);
