193573d72a74cb750f691b0ace7bcc19a3ae6c82
Kamil T <trzaskomakamil@gmail.com>
2019-10-21 19:25:10 +0200

    Slide-out Drawer and ChromePicker added to NewPaletteForm

--- src/components/NewPaletteForm.js
+++ src/components/NewPaletteForm.js
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
+import clsx from 'clsx'
+import { useStyles } from '../styles/NewPaletteFormStyles'
+import Drawer from '@material-ui/core/Drawer'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import Button from '@material-ui/core/Button'
+import { ChromePicker } from 'react-color'
 
 const NewPaletteForm = () => {
+  const classes = useStyles()
+  const [open, setOpen] = useState(false)
+
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
+
   return (
-    <div>
-      <h1>New Palette</h1>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position='fixed'
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open
+        })}>
+        <Toolbar>
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            onClick={handleDrawerOpen}
+            edge='start'
+            className={clsx(classes.menuButton, open && classes.hide)}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' noWrap>
+            Persistent drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant='persistent'
+        anchor='left'
+        open={open}
+        classes={{
+          paper: classes.drawerPaper
+        }}>
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <Typography variant='h4'>Design Your Palette</Typography>
+        <div>
+          <Button variant='contained' color='secondary'>
+            Clear Palette
+          </Button>
+          <Button variant='contained' color='primary'>
+            Random Palette
+          </Button>
+        </div>
+        <ChromePicker color='purple' />
+        <Button variant='contained' color='primary'>
+          Add Color
+        </Button>
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open
+        })}>
+        <div className={classes.drawerHeader} />
+      </main>
     </div>
   )
 }
